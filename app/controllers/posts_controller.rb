6e4836ade7afbcdd8806082c9a41035f2adb34e1
KoukiKishida <kk.so.m.84@icloud.com>
2021-03-26 16:04:35 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(id: "DESC") # すべてのレコードを@postsに代入
+    @posts = Post.all.order(id: "DESC")
   end
-
 
   def create
     Post.create(content: params[:content])
@@ -11,7 +10,7 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
-    if post.checked 
+    if post.checked
       post.update(checked: false)
     else
       post.update(checked: true)
